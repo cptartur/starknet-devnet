45150476841b64a093bf4e9e1ca33ca24a3b3d39
Artur Michalek <artur.michalek@swmansion.com>
2023-01-03 13:10:01 +0100

    fixup! Add modified RPC specs

--- test/support/assertions.py
+++ test/support/assertions.py
@@ -9,13 +9,13 @@ from jsonschema import RefResolver, validate
 def assert_valid_schema(data, schema_file):
     """Checks whether the given data matches the schema"""
 
-    schema = _load_json_schema(schema_file)
+    schema = load_json_schema(schema_file)
     schema_dir = join(dirname(__file__), "schemas")
     resolver = RefResolver(base_uri="file://" + schema_dir + "/", referrer=schema)
     return validate(data, schema, resolver=resolver)
 
 
-def _load_json_schema(filename):
+def load_json_schema(filename):
     """Loads the given schema file"""
 
     relative_path = join("schemas", filename)
