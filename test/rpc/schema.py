dfcc86ae0e308798cc66b674cd02e5cbccb6b9b3
Artur Michalek <artur.michalek@swmansion.com>
2023-01-05 14:42:38 +0100

    Add comments, cleanup

--- test/rpc/schema.py
+++ test/rpc/schema.py
@@ -1,17 +1,29 @@
+from functools import lru_cache
+
 from test.support.assertions import load_json_schema
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 from jsonschema import validate
 
-specs_json = load_json_schema("starknet_api_openrpc.json")
-schemas = specs_json["components"]["schemas"]
-methods = {method["name"]: method for method in specs_json["methods"]}
 
-for schema in schemas.values():
-    if "required" not in schema and "properties" in schema:
-        schema["required"] = [prop for prop in schema["properties"].keys()]
-    if "allOf" in schema or "anyOf" in schema:
-        schema["unevaluatedProperties"] = False
+# Cache the function result so schemas are not reloaded from disk on every call
+@lru_cache
+def load_schemas() -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    specs_json = load_json_schema("starknet_api_openrpc.json")
+    schemas = specs_json["components"]["schemas"]
+    methods = {method["name"]: method for method in specs_json["methods"]}
+
+    for schema in schemas.values():
+        # Newer version of the rpc (above 0.45.0) have required properly defined.
+        # Once we start targeting them, this can be removed.
+        if "required" not in schema and "properties" in schema:
+            schema["required"] = [prop for prop in schema["properties"].keys()]
+
+        # Ensures validation fails in case of extra fields not matched by any of `allOf` / `anyOf` branches.
+        if "allOf" in schema or "anyOf" in schema:
+            schema["unevaluatedProperties"] = False
+
+    return methods, schemas
 
 
 def assert_valid_rpc_schema(data: Dict[str, Any], method_name: str):
@@ -24,21 +36,23 @@ def _schema_for_method(name: str) -> Dict[str, Any]:
     Return a dict structured like
     {
         // base schema
+        // ...
         "components": {
             "schemas": {
+                // rest of the schemas
                 // ...
             }
         }
     }
-    for the use in json schema validation
+    for the use in json schema validation.
     """
-    # base_schema = schemas[name]
+
+    methods, schemas = load_schemas()
     base_schema = methods[name]["result"]["schema"]
 
     if all(i not in base_schema for i in ("allOf", "anyOf", "oneOf")):
+        # This has to be done here, because setting additionalProperties = False in
+        # load_schemas doesn't work with `allOf` etc. properly.
         base_schema["additionalProperties"] = False
-
-    # new_schemas = copy.deepcopy(schemas)
-    # del new_schemas[name]
 
     return {**base_schema, "components": {"schemas": schemas}}
